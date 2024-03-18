412f1ea6ba82cb3dd88832ab4376f2cdadf75cd3
awenafunagain <awen.bourdon@gmail.com>
2024-03-18 15:50:51 +0100

    Create get_time.js
    Script pour se situer dans le temps

---
+++ get_time.js
@@ -0,0 +1,6 @@
+const now = new Date();
+const heure = now.getHours();
+const minute = now.getMinutes();
+const seconde = now.getSeconds();
+
+console.log("Il est " + heure + ":" + minute + ":" + seconde);
